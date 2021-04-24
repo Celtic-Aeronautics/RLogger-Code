e71668efaff8ede2deca8c736e1df9a4753543f4
nacho <nachocpol@gmail.com>
2021-04-24 13:38:48 +0100

    Testing SD info!

--- src/Storage/SD/SDCard.cpp
+++ src/Storage/SD/SDCard.cpp
@@ -17,5 +17,12 @@ bool SDCard::Init(uint8_t chipSelect)
         return false;
     }
 
+    
+    csd_t csd = {};
+    sd.card()->readCSD(&csd);
+    uint32_t capacity = sdCardCapacity(&csd);
+
+    Serial.print("Total SDCard capacity:"); Serial.println((float)capacity * 0.000512f);
+
     return true;
 }
